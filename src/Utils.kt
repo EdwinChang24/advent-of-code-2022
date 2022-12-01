8d7fc7a22d15a171e3ccb0a829727127654abee0
Edwin Chang <edwinchang2024@gmail.com>
2022-11-30 23:06:19 -0800

    Update structure

--- src/Utils.kt
+++ src/Utils.kt
@@ -3,13 +3,22 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
- * Reads lines from the given input txt file.
+ * Reads lines from the given input `.txt` file. The file will be read from `src/name/name.txt`.
+ *
+ * @param name The name of the file, with the `.txt` extension removed.
  */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+fun readInput(name: String) = File(File("src", name), "$name.txt").readLines()
 
 /**
- * Converts string to md5 hash.
+ * Reads lines from the given input `.txt` file.
+ *
+ * @param dir The directory, under `src`, to get the file from.
+ * @param name The name of the file, with the `.txt` extension removed.
+ */
+fun readInput(dir: String, name: String) = File(File("src", dir), "$name.txt").readLines()
+
+/**
+ * Gets the MD5 hash of this string.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
